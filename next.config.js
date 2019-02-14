99dbc1b8f7c8303cd21af5c1a4ff8879c87f9528
Trần Đức Tiến <tientran0019@gmail.com>
2019-02-14 16:08:26 +0700

    add config .env

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable import/no-extraneous-dependencies */
 /* --------------------------------------------------------
 * Author Trần Đức Tiến
@@ -6,12 +7,14 @@
 *
 * Created: 2019-02-11 11:51:01
 *------------------------------------------------------- */
+require('dotenv').config();
 
 const lessToJS = require('less-vars-to-js');
 const fs = require('fs');
 const path = require('path');
 const withPlugins = require('next-compose-plugins');
 const { PHASE_PRODUCTION_BUILD } = require('next/constants');
+const Dotenv = require('dotenv-webpack');
 
 const withLessExcludeAntd = require('./next-less.config');
 
@@ -27,20 +30,40 @@ if (typeof require !== 'undefined') {
 
 
 module.exports = withPlugins([
-	[withLessExcludeAntd, {
-		cssModules: true,
-		cssLoaderOptions: {
-			importLoaders: 1,
-			localIdentName: '[path]___[local]___[hash:base64:5]',
-		},
-		lessLoaderOptions: {
-			javascriptEnabled: true,
-			modifyVars: themeVariables,
-		},
-		[PHASE_PRODUCTION_BUILD]: {
+	[
+		withLessExcludeAntd,
+		{
+			cssModules: true,
 			cssLoaderOptions: {
-				localIdentName: '[hash:base64:8]',
+				importLoaders: 1,
+				localIdentName: '[path]___[local]___[hash:base64:5]',
+			},
+			lessLoaderOptions: {
+				javascriptEnabled: true,
+				modifyVars: themeVariables,
+			},
+			[PHASE_PRODUCTION_BUILD]: {
+				cssLoaderOptions: {
+					localIdentName: '[hash:base64:8]',
+				},
 			},
 		},
-	}],
-]);
+	],
+],
+{
+	webpack: config => {
+		config.plugins = config.plugins || [];
+
+		config.plugins = [
+			...config.plugins,
+
+			// Read the .env file
+			new Dotenv({
+				path: path.join(__dirname, '.env'),
+				systemvars: true,
+			}),
+		];
+
+		return config;
+	},
+});
