2fd76120ed7ed67fbdfd714b80d33a86e12450f4
Trần Đức Tiến <tientran0019@gmail.com>
2019-02-14 15:41:15 +0700

    update nextjs version

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* --------------------------------------------------------
 * Author Trần Đức Tiến
 * Email ductienas@gmail.com
@@ -6,21 +7,22 @@
 * Created: 2019-02-11 11:51:01
 *------------------------------------------------------- */
 
-const lessToJS = require('less-vars-to-js')
-const fs = require('fs')
-const path = require('path')
+const lessToJS = require('less-vars-to-js');
+const fs = require('fs');
+const path = require('path');
 const withPlugins = require('next-compose-plugins');
-const withLessExcludeAntd = require('./next-less.config')
 const { PHASE_PRODUCTION_BUILD } = require('next/constants');
+
+const withLessExcludeAntd = require('./next-less.config');
 
 // Where your antd-custom.less file lives
 const themeVariables = lessToJS(
-	fs.readFileSync(path.resolve(__dirname, './src/theme/variables.less'), 'utf8')
-)
+	fs.readFileSync(path.resolve(__dirname, './src/theme/variables.less'), 'utf8'),
+);
 
 // fix: prevents error when .less files are required by node
 if (typeof require !== 'undefined') {
-	require.extensions['.less'] = file => { }
+	require.extensions['.less'] = file => { };
 }
 
 
@@ -33,7 +35,7 @@ module.exports = withPlugins([
 		},
 		lessLoaderOptions: {
 			javascriptEnabled: true,
-			modifyVars: themeVariables
+			modifyVars: themeVariables,
 		},
 		[PHASE_PRODUCTION_BUILD]: {
 			cssLoaderOptions: {
