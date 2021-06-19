f0070154f94315bf4ae3e1a41d09b4a5df7fcf38
tientran <tientran0019@gmail.com>
2021-06-19 12:36:01 +0700

    webpack

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* --------------------------------------------------------
 * Author Tien Tran
 * Email tientran0019@gmail.com
@@ -5,21 +6,32 @@
 *
 * Created: 2021-06-17 12:29:42
 *------------------------------------------------------- */
+const path = require('path');
 
 const withAntdLess = require('next-plugin-antd-less');
-const path = require('path');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true',
+});
+const lessToJS = require('less-vars-to-js');
+const fs = require('fs');
+
 const loadEnvConfig = require('./bin/env');
 
 loadEnvConfig();
 
-module.exports = withAntdLess({
-	// modifyVars: { '@primary-color': 'red' },
-	lessVarsFilePath: './src/styles/variables.less',
-	lessVarsFilePathAppendToEndOfContent: true,
+const antdVariables = lessToJS(fs.readFileSync(path.resolve(__dirname, 'src/styles/variables.less'), 'utf8'));
+
+module.exports = withBundleAnalyzer(withAntdLess({
+	modifyVars: {
+		'hack': 'true;@import "~antd/lib/style/themes/compact.less";',
+		...antdVariables,
+	},
+	// lessVarsFilePath: './src/styles/variables.less',
+	// lessVarsFilePathAppendToEndOfContent: true,
 	// optional https://github.com/webpack-contrib/css-loader#object
 	cssLoaderOptions: {
 		modules: {
-			auto: /\.module\.\w+$/i,
+			// auto: /\.module\.\w+$/i,
 			localIdentName: process.env.MODE !== 'production' ? '[folder]__[local]__[hash:4]' : '[hash:8]',
 			localIdentContext: path.resolve(__dirname, 'src'),
 		},
@@ -30,4 +42,4 @@ module.exports = withAntdLess({
 	webpack(config) {
 		return config;
 	},
-});
+}));
